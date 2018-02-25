556e6b61f4382cc84d9ecae4f6e38096229abcfb
stolbovd <stolbovd@gmail.com>
2018-02-25 18:34:56 +0500

    первая попытка

--- html5/js/first.js
+++ html5/js/first.js
@@ -1,1 +1,8 @@
 console.log("hello world");
+
+const N = 6;
+var W = []; // array[1..N, 1..N] of integer
+var active; // array[1..N] of boolean
+var R, P; // array[1..N] of integer
+var i, j, min, kMin; // integer
+
